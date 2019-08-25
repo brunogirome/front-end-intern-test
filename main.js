b1b18cb634943f5713b0eb2e066569c97349ddef
Bruno Girome <brunogirome@gmail.com>
2019-08-25 14:02:03 -0300

    Polimento do CSS e alterações no JS

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Adicionamos nosso código dentro de uma função anônima para evitar
 // variáveis globais.
-(function() {
-  // Elemento principal do formulário
+(function () {
+  // Elemento principal do formulário.
   const contactForm = document.getElementById('contact');
 
-  // Campos do formulário
+  // Campos do formulário.
   const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
   const messageInput = document.getElementById('message');
@@ -12,21 +12,40 @@
   // "addEventListener" instrui o navegador a executar uma função sempre
   // que um evento específico acontece. Nesse caso, a função abaixo será
   // executada toda vez que o botão de envio for pressionado.
-  contactForm.addEventListener('submit', function() {
-    // A propriedade "value" contém o valor atual de um input ou textarea
+  contactForm.addEventListener('submit', function () {
+    // Variáveis dos valores correspondentes aos campos do formulário.
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
 
-    // A propriedade "length" contém o tamanho (em caracteres) de uma
-    // uma string. Toda string contém essa propriedade.
-    const nameLength = nameValue.length;
+    // Variáveis armazenando "true" ou "false" caso o conteúdo do campo
+    // for maior que zero (no caso, vazio ou não).
+    const nameLength = nameValue.length > 0;
+    const emailLength = emailValue.length > 0;
+    const messageLength = messageValue.length > 0;
 
-    // "window" é um objeto global que representa a janela (ou aba) do
-    // navegador que está executando o código do seu site. O método
-    // "alert" simplesmente mostra um aviso para o usuário contendo a
-    // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+    //Verificando todos os valores de uma só vez com operador && (AND).
+    if (nameLength && emailLength && messageLength) {
+      window.alert(`
+        Agradecemos o contato ${nameValue}!
+        Iremos respondê-l@ pelo o seu email: ${emailValue}\n
+        Conteúdo da sua mensagem: "${messageValue}"
+        `);
+    } else {
+      // Variável que armazena e concatena a mensagem de erro que será
+      // exibida caso o usuário deixe algum campo em branco.
+      let alertMessage = '';
 
-    // Altere e complete essa função para validar os campos do formulário
-    // de acordo com as especificações do teste. Boa sorte!
+      if (nameLength == false)
+        alertMessage += 'Por gentileza, digite seu nome.\n';
+
+      if (!emailLength)
+        alertMessage += 'Por gentileza, digite seu e-mail.\n';
+
+      if (!messageLength)
+        alertMessage += 'Por gentileza, digite uma mensagem.\n';
+
+      window.alert(alertMessage);
+    }
   });
 })();
